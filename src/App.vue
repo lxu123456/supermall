321d4b87fd2bd622b154db75c5c2e43b27370a63
e <xuliang@china.kjlink.com>
2020-12-05 23:05:24 +0800

    商城框架搭建

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
-  </div>
+	  <router-view/>
+	  <main-tab-bar/>
+  </div> 
 </template>
 
 <script>
-
+import MainTabBar from 'components/content/mainTabbar/MainTabBar.vue'
 export default {
   name: 'App',
   components: {
+	  MainTabBar
   }
 }
 </script>
 
 <style>
+	@import url("assets/css/base.css");
 </style>
