b6438fbdb88618a59cfdc85080b1aba8a183f33f
e <xuliang@china.kjlink.com>
2020-12-06 16:08:20 +0800

    nav-bar

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,22 @@
 <template>
-  <h2>首页</h2>
+	<div id="home">
+		<nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+	</div>
 </template>
 
 <script>
-  export default {
-    name: "Home"
-  }
+	import NavBar from '../../components/common/navbar/NavBar'
+	export default {
+		name: "Home",
+		components:{
+			NavBar
+		}
+	}
 </script>
 
 <style scoped>
-
+	.home-nav{
+		background-color: var(--color-tint);
+		color: #FFFFFF;
+	}
 </style>
