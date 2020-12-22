8eeb7741d09dee2b9b2021d66cb710f628b2f5b0
e <xuliang@china.kjlink.com>
2020-12-22 22:27:39 +0800

    详情页导航

--- src/views/detail/childrencomponents/DetailNavBar.vue
+++ src/views/detail/childrencomponents/DetailNavBar.vue
@@ -1,6 +1,19 @@
 <template>
 	<div>
-		<nav-bar><div slot="center">详情页</div></nav-bar>
+		<nav-bar>
+			<div slot="left" class="back" @click="backClick">
+				<img src="~assets/img/common/back.svg" alt="" />
+			</div>
+			<div slot="center" class="title">
+				<div :key="item.label" 
+					 v-for="(item,index) in titles" 
+					 class="title-item" 
+					 :class="{active: index === currentIndex}"
+					 @click="itemClick(index)">
+				  {{item}}
+				</div>
+			</div>
+		</nav-bar>
 	</div>
 </template>
 
@@ -10,9 +23,36 @@
 		name:"DetailNavBar",
 		components:{
 			NavBar
+		},
+		data(){
+			return {
+				titles:['参数','详情','评价','推荐'],
+				currentIndex:0
+			}
+		},
+		methods:{
+			itemClick(index){
+				this.currentIndex =index
+			},
+			backClick(){
+				this.$router.go(-1)
+			}
 		}
 	}
 </script>
 
 <style>
+	.title{
+		display: flex;
+		font-size: 13px;
+	}
+	.title-item{
+		flex: 1;
+	}
+	.active{
+		color: var(--color-high-text);
+	}
+	.back img{
+		margin-top: 10px;
+	}
 </style>
