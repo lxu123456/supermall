9eb8579ee33c8e6ca1c5b095b212b34899b88d4e
e <xuliang@china.kjlink.com>
2020-12-22 23:23:17 +0800

    详情页轮播图

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,25 +1,37 @@
 <template>
 	<div id="detail">
 		<detail-nav-bar/>
+		<detail-swiper :topImages="topImages" />
 	</div>
 </template>
 
 <script>
 	
 	import DetailNavBar from './childrencomponents/DetailNavBar.vue'
+	import DetailSwiper from './childrencomponents/DetailSwiper.vue'
+	
+	import {getDetail} from '../../network/detail.js'
 	
 	export default{
 		name:"Detail",
 		data(){
 			return {
-				iid:null
+				iid:null,
+				topImages:[]
 			}
 		},
 		components:{
-			DetailNavBar
+			DetailNavBar,
+			DetailSwiper
 		},
 		created() {
+			//1.商品id保存
 			this.iid = this.$route.params.iid
+			//2.根据id获取商品详情
+			getDetail(this.iid).then(res =>{
+				//2.1获取顶部轮播图数据
+				this.topImages = res.result.itemInfo.topImages
+			})
 		}
 	}
 </script>
