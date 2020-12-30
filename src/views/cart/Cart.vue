3bb08582b9e6b195dbc5e49e25a18928c08f0880
e <xuliang@china.kjlink.com>
2020-12-30 23:00:27 +0800

    购物车布局

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,21 +1,29 @@
 <template>
-  <nav-bar class="cart-nav-bar">
-	  <div slot="center">购物车({{$store.state.carList.length}})</div>
-  </nav-bar>
+	<div class="cart1">
+		<nav-bar class="cart-nav-bar">
+			  <div slot="center">购物车({{$store.state.carList.length}})</div>
+		</nav-bar>
+		<cart-list></cart-list>
+	</div>
 </template>
 
 <script>
   import NavBar from '../../components/common/navbar/NavBar.vue'
+  import CartList from './childrenComponents/CartList.vue'
   
   export default {
     name: "Cart",
 	components:{
-		NavBar
+		NavBar,
+		CartList
 	}
   }
 </script>
 
 <style scoped>
+	.cart1{
+		height: 100vh;
+	}
 	.cart-nav-bar{
 		background-color: var(--color-tint);
 		color: #FFFFFF;
---
+++ src/views/cart/childrenComponents/CartList.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="cart-list">
+		<scroll class="content2" ref="scroll">
+			<cart-list-item v-for="item in $store.state.carList" :key="item.iid" :itemInfo="item" />
+		</scroll>
+	</div>
+</template>
+
+<script>
+	
+	import CartListItem from './CartListItem.vue'
+	import Scroll from '../../../components/common/scroll/Scroll.vue'
+	
+	export default{
+		name:"cartList",
+		components:{
+			CartListItem,
+			Scroll
+		},
+		activated() {
+			this.$refs.scroll.refresh()
+		}
+	}
+</script>
+
+<style>
+	.cart-list{
+		height: 100vh;
+	}
+	.content2{
+		height: calc(100% - 44px - 49px);
+		overflow: hidden;
+	}
+</style>
